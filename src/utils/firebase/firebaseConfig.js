bbb806f6e3dbdf5517f896eba66325a2ac7f2f97
Baptiste LECHAT <baptistelechat@outlook.fr>
2022-01-27 17:15:03 +0100

    🔒️ Update firebase-config.js

--- src/utils/firebase/firebaseConfig.js
+++ src/utils/firebase/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBbMFAq2pdMmol8QpSPeIU28onRSWe9ccc",
-    authDomain: "react-gdrive-api.firebaseapp.com",
-    projectId: "react-gdrive-api",
-    storageBucket: "react-gdrive-api.appspot.com",
-    messagingSenderId: "424492673114",
-    appId: "1:424492673114:web:885582a17b62431fa65063",
-    measurementId: "G-5D497K5HXH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
